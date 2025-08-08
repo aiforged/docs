a143b9d6ad4c1d48a36431b576c1d756e8511f4a
adLarcAI <74778708+AdriaanLarcai@users.noreply.github.com>
2025-08-08 13:46:22 +0200

    Update website.js

--- scripts/website.js
+++ scripts/website.js
@@ -1,13 +1,1 @@
-gitbook.events.bind("start", function() {
-    var searchContainer = $('<div id="search-container"></div>')
-        .css({ padding: "10px" });
-    $(".book-summary").prepend(searchContainer);
-
-    $('head').append('<link rel="stylesheet" href="pagefind/pagefind-ui.css">');
-    $.getScript("pagefind/pagefind-ui.js", function() {
-        new PagefindUI({
-            element: "#search-container",
-            showImages: false
-        });
-    });
-});
+// Located in .gitbook/scripts
